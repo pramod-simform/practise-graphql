dfef398dda1b185a2c4e94a42129cc86eb130e00
pramod tolani <pramod@simformsolutions.com>
2023-08-08 13:26:25 +0530

    chore: configure db

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
+import createConnection from './db/connection.js';
 
 import { QueryTypeDef } from './graphQl/queries/index.query.js';
 import { Resolvers } from './graphQl/resolvers/index.resolver.js';
@@ -8,6 +9,8 @@ import { CommentTypeDef } from './graphQl/schema/post/comment.schema.js';
 import { LikeTypeDef } from './graphQl/schema/post/like.schema.js';
 import { PostTypeDef } from './graphQl/schema/post/post.schema.js';
 import { UserTypeDef } from './graphQl/schema/user/user.schema.js';
+
+createConnection();
 
 const resolvers = {
   ...Resolvers
