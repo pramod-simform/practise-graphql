bc176e397b97e683291c351fc3da1e79f77e8392
pramod tolani <pramod@simformsolutions.com>
2023-09-21 17:12:27 +0530

    feat: change the mutation arguments names as per mapped values

--- src/graphQl/mutations/index.mutation.ts
+++ src/graphQl/mutations/index.mutation.ts
@@ -1,13 +1,13 @@
 export const MutationTypeDef = `
   type Mutation {
-    createUser(name: String!, email: String!, age: Int!, location: String!): User
-    createPost(userId: ID!, title: String!, content: String!): Post
-    createComment(userId: ID!, postId: ID!, content: String!): Comment
+    createUser(fullName: String!, username: String!, ageGroup: Int!, address: String!): User
+    createPost(userId: ID!, heading: String!, description: String!): Post
+    createComment(userId: ID!, postId: ID!, note: String!): Comment
     createLike(userId: ID!, postId: ID!): Like
 
-    updateUser(_id: ID!, name: String!, email: String!, age: Int!, location: String!): User
-    updatePost(_id: ID!, userId: ID!, title: String!, content: String!): Post
-    updateComment(_id: ID!, userId: ID!, postId: ID!, content: String!): Comment
-    updateLike(_id: ID!): Boolean
+    updateUser(id: ID!, fullName: String!, username: String!, ageGroup: Int!, address: String!): User
+    updatePost(id: ID!, userId: ID!, heading: String!, description: String!): Post
+    updateComment(id: ID!, userId: ID!, postId: ID!, note: String!): Comment
+    updateLike(id: ID!): Boolean
   }
 `;
