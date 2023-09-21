cd60d7994eafc7906c65c5e64508351232c7e54d
pramod tolani <pramod@simformsolutions.com>
2023-09-21 17:12:53 +0530

    refactor: change something related to contact details field value for create user method

--- src/db/services/user.service.ts
+++ src/db/services/user.service.ts
@@ -58,7 +58,7 @@ export const createUser = async ({
     email,
     age,
     location,
-    contactDetails,
+    ...(contactDetails ? { contactDetails } : {}),
   });
 
   return (await UserObj.save()).toObject();
