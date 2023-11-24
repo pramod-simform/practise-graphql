63ac9fcee8719ccc7c6a2c7d8bfd0b6cdbb3c53b
pramod tolani <pramod@simformsolutions.com>
2023-11-24 15:22:24 +0530

    fix: fixed an issue related to create user mutation subscription

--- src/graphQl/resolvers/mutations/user.mutation.ts
+++ src/graphQl/resolvers/mutations/user.mutation.ts
@@ -6,8 +6,9 @@ import { getFieldsMappedData } from "../../../utils/helper.js";
 
 export const UserMutations = {
   createUser: async (_: any, args: IUser) => {
-    pubsub.publish("USER_CREATED", { createUser: args });
-    return getFieldsMappedData("users", await createUser(args));
+    let data =  getFieldsMappedData("users", await createUser(args));
+    pubsub.publish("USER_CREATED", { createUser: data });
+    return data;
   },
 
   updateUser: async (_: any, args: IUser) => {
--- src/graphQl/subscriptions/index.subscription.ts
+++ src/graphQl/subscriptions/index.subscription.ts
@@ -1,5 +1,15 @@
 export const SubscriptionTypeDef = `
+  type SubUserData {
+    id: ID!
+    fullName: String!
+    username: String!
+    ageGroup: Int!
+    address: String!
+    posts: [Post!]!
+    createdAt: Date
+    contactInfo: JSON
+  }
   type Subscription {
-    createUser: User
+    createUser: SubUserData
   }
 `;
